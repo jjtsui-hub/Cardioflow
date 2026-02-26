074cb4e7dcc8c7d63190a914b7f6280c5c778308
jjtsui-hub <jjtsui@gmail.com>
2026-02-26 06:01:52 +0600

    Add files via upload

---
+++ migrations/1/down.sql
@@ -0,0 +1,18 @@
+
+DROP INDEX idx_diet_logs_date;
+DROP INDEX idx_diet_logs_user_id;
+DROP INDEX idx_activity_logs_date;
+DROP INDEX idx_activity_logs_user_id;
+DROP INDEX idx_medication_logs_user_id;
+DROP INDEX idx_medications_user_id;
+DROP INDEX idx_vital_logs_logged_at;
+DROP INDEX idx_vital_logs_user_id;
+DROP INDEX idx_health_goals_user_id;
+DROP INDEX idx_user_profiles_user_id;
+DROP TABLE diet_logs;
+DROP TABLE activity_logs;
+DROP TABLE medication_logs;
+DROP TABLE medications;
+DROP TABLE vital_logs;
+DROP TABLE health_goals;
+DROP TABLE user_profiles;
